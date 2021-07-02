61b7482e52fabc9efb5d0bc85d360a6bad2cd973
Mariano Sciacco <34033090+Maxelweb@users.noreply.github.com>
2021-07-02 21:08:23 +0200

    Create gesture-assistant.py

---
+++ classes/gesture-assistant.py
@@ -0,0 +1,16 @@
+"""
+ 
+ GestureAssistant
+ 
+ ---------------
+ This class is used as an adapter to translate the gesture recognized into a google assistant command
+
+ ---------------
+
+"""
+
+
+class GestureAssistant:
+    
+    def __init__(self):
+        self.frame_buffer = None
